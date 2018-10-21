d7863ecd6cf85f44ef41c4692aa36213328da3ba
Jayden Navarro <jaynavar@stanford.edu>
2018-10-21 10:52:59 -0700

    Added numIters as optional argument to Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 
+import argparse
 import Player
 
-def main():
+DEFAULT_ITERS = 100
+
+def main(numIters=DEFAULT_ITERS):
    """Primary test harness."""
 
    # run the baseline random player
    p = Player.BaselineRandomPlayer()
-   p.run(numIters=100, printStats=True)
+   p.run(numIters=numIters, printStats=True)
 
 if __name__ == '__main__':
-   main()
+   parser = argparse.ArgumentParser()
+   parser.add_argument('numIters', nargs='?', default=DEFAULT_ITERS,
+                       help=('number of simulation runs, default %d' %
+                             DEFAULT_ITERS))
+   args = parser.parse_args()
+
+   main(numIters=int(args.numIters))
