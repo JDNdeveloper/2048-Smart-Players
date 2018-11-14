e783f629ef80bff5b55b54b9202d452fa60c6ec8
magdy <magdyksaleh@gmail.com>
2018-11-14 11:29:14 -0800

    Expectimax player covers all positions

--- src/ExpectiMaxPlayer.py
+++ src/ExpectiMaxPlayer.py
@@ -3,7 +3,6 @@ import random
 import copy
 
 ########################################################################################################################
-# TODO: change tile spawning to consider all possible spawns
 # TODO: implement heuristics
 ########################################################################################################################
 class ExpectiMaxPlayer(Player):
@@ -18,7 +17,7 @@ class ExpectiMaxPlayer(Player):
 
     def isGameOver(self, board):
         """Check if Game is over"""
-        if len([(row, col) for row in range(self.m.SIZE) for col in range(self.m.SIZE) if board[row][col] == None]) > 0:
+        if len(self.getOpenPos(board)) > 0:
             return False
         for i in range(self.m.SIZE):
             # check for consecutive numbers in all rows and cols
@@ -43,11 +42,14 @@ class ExpectiMaxPlayer(Player):
         """Get Current sum of elements on the board"""
         return sum([sum(filter(None, list)) for list in board])
 
+    def getOpenPos(self, board):
+        return [(row, col) for row in range(self.m.SIZE) for col in range(self.m.SIZE) if board[row][col] == None]
+
     def randomFill(self, board, move):
         """Randomly fill an open position on the board.
         The fill values and probability distribution is defined above.
         """
-        openPos = [(row, col) for row in range(self.m.SIZE) for col in range(self.m.SIZE) if board[row][col] == None]
+        openPos = self.getOpenPos(board)
         if len(openPos) == 0: return board
         (row, col) = random.choice(openPos)
         board[row][col] = move
@@ -58,7 +60,11 @@ class ExpectiMaxPlayer(Player):
         if index == 0:
             return self.m.makeMove(move, modifyState=False, returnBoard=True)[2]
         else: 
-            return self.randomFill(board, move)
+            row, col = move[1]
+            val = move[0]
+            board[row][col] = val
+            return board
+            
 
 
     def adjustLegalMoves(self, board):
@@ -71,6 +77,10 @@ class ExpectiMaxPlayer(Player):
             self.lastBoard = copy.deepcopy(board)
             self.legalMoves_agent = self.m.MOVES
         return
+    
+    def maxTile(self, board):
+      """Returns value of maximum tile on the board."""
+      return max(val for row in board for val in row)
 
     def getMove(self, board, score):
         """
@@ -85,8 +95,14 @@ class ExpectiMaxPlayer(Player):
             if self.isGameOver(board):
                 return ("", self.getScore(board), index)
             ######################
+            # legalMoves = self.legalMoves_agent if index == 0 else [(2, random.choice(self.getOpenPos(board)))] + [(4, random.choice(self.getOpenPos(board)))]
+            
+            # if len(self.getOpenPos(board)) < 8 and index != 0:
+            #     legalMoves = [(2, pos) for pos in self.getOpenPos(board)] + [(4, pos) for pos in self.getOpenPos(board)] #based on player vs. random gen
+                
+            # print legalMoves
 
-            legalMoves = self.legalMoves_agent if index == 0 else [2, 4] #based on player vs. random gen
+            legalMoves = self.legalMoves_agent if index == 0 else [(2, pos) for pos in self.getOpenPos(board)] + [(4, pos) for pos in self.getOpenPos(board)]
             newBoards = [self.generateNextMove(board, index, move) for move in legalMoves]
             
             if index == 0: #agent case
@@ -105,10 +121,18 @@ class ExpectiMaxPlayer(Player):
         self.adjustLegalMoves(board)
         bestMove = recurse(self.m.getState()[0], 0, self.depth)
         self.lastMove = bestMove[0]
+        print board
+        print board[0][0]
+        print "BestMove: {}".format(bestMove[0])
         return self.lastMove
     
     def evalFunction(self, board): #Todo
-        return self.getScore(board)
+        retVal = len(self.getOpenPos(board))
+        if self.maxTile(board) == board[0][0]:
+            retVal *= 1000
+        elif not (self.maxTile(board) in [board[i][0] for i in range(len(board))]):
+            retVal /= self.maxTile(board)
+        return retVal #+0.2*self.getScore(board)
         
         
         
