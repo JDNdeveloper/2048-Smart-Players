056a1d19bf0e7551a48b474aca1e99e2cf7b460d
Jayden Navarro <jaynavar@stanford.edu>
2018-12-06 23:51:18 +0000

    Changed runTrials to use new sequence files

--- src/runTrials.sh
+++ src/runTrials.sh
@@ -2,7 +2,8 @@
 
 SIZE=$1
 ID=$2
-SEQUENCES=sequences/exp-2-sequence.yaml
+BSIZE=${SIZE}x${SIZE}
+SEQUENCES=sequences/${BSIZE}_sequences.yaml
 (set -x; time python -u Main.py --player ql --sequences \
     ${SEQUENCES} --size ${SIZE}) 2>&1 \
-    | tee data/${SIZE}x${SIZE}_data_${ID}.txt
+    | tee data/${BSIZE}_data_${ID}.txt
