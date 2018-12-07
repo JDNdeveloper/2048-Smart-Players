b405916fc9b1d43000d9c704e0c7bc47227b83c7
Jayden Navarro <jdndeveloper@gmail.com>
2018-12-06 19:44:05 -0800

    Added num iters param to EM trials script

--- src/runEMTrials.sh
+++ src/runEMTrials.sh
@@ -2,10 +2,11 @@
 
 SIZE=$1
 ID=$2
-DEPTH=$3
-PROB_CUTOFF=$4
+NUM_ITERS=$3
+DEPTH=$4
+PROB_CUTOFF=$5
 BSIZE=${SIZE}x${SIZE}
 SEQUENCES=sequences/${BSIZE}_sequences.yaml
-(set -x; time python -u Main.py --player em --depth ${DEPTH} \
+(set -x; time python -u Main.py ${NUM_ITERS} --player em --depth ${DEPTH} \
     --probCutoff ${PROB_CUTOFF} --size ${SIZE}) 2>&1 \
     | tee data/${BSIZE}_EM_data_${ID}.txt
